ce476efa625a482c00e29214b18df852dc77a81a
Prakash <prakash82993@gamail.com>
2024-08-22 23:46:55 +0530

    display needs to be changedgit add .!

---
+++ queue/circularQueue_array.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define ARRAYSIZE 10
+int cqueue[ARRAYSIZE];
+int ch,x;
+int front=-1;
+int rear=-1;
+void enqueue(int x)
+{
+	if(front=(rear+1)%ARRAYSIZE)
+	{
+		printf("\nqueue is full!!!");
+	}
+	else
+	{
+		rear=(rear+1)%ARRAYSIZE;
+		cqueue[rear]=x;
+		if(front=-1)
+		{
+			front=front+1;
+		}
+	}
+}
+void dequeue()
+{
+	if(front==-1 && rear==-1)
+	{
+		printf("\nqueue is empty!!!");
+	}	
+	else
+	{
+		printf("the dequeue element is %d",cqueue[front]);
+		if(front==rear)
+		{
+			front=-1;
+			rear=-1;
+		}
+		else
+		{
+			front=(front+1)%ARRAYSIZE;
+		}
+	}
+}
+
+void display()
+{
+	int i;
+	if(front==-1 && rear==-1)
+	{
+		printf("!!queue is already empty nothing to display!!!");
+	}
+	else
+	{
+		for(i=front;i<=rear;i++)
+		{
+			printf("%d\t",cqueue[i]);
+		}
+	}
+}
+void main()
+{
+	while(1)
+	{
+		
+		printf("\n1)enqueue\n2)dequeue\n3)display\n4)exit\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+		
+			case 1:
+					printf("\nenter the element:\t");
+					scanf("%d",&x);
+					enqueue(x);
+					break;
+			case 2:
+					dequeue();
+					break;
+			case 3: display();
+					break;
+			case 4:
+					exit(0);
+			default:
+					printf("\nwrong choise!!!!");	
+		}
+	}
+}
